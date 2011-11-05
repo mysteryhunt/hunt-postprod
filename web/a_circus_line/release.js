2e033045ff34661831a92397fbbd72623cf9dd36
C. Scott Ananian <cscott@cscott.net>
2011-11-05 19:58:16 -0400

    More bits of the index into a rewritable javascript file.

---
+++ web/a_circus_line/release.js
@@ -0,0 +1,77 @@
+var ii = document.getElementById('index-image');
+ii.innerHTML = ''+
+'  <img src="1S-P0.png" usemap="#map" />'+
+'  <img src="1S-P1.png" usemap="#map" />'+
+'  <img src="1S-P2.png" usemap="#map" />'+
+'  <img src="1S-P3.png" usemap="#map" />'+
+'  <img src="1S-P4.png" usemap="#map" />'+
+'  <img src="1S-P5.png" usemap="#map" />'+
+'  <img src="1S-P6.png" usemap="#map" />'+
+'  <img src="1S-P7.png" usemap="#map" />'+
+'  <img src="1S-P8.png" usemap="#map" />'+
+'  <img src="1S-P9.png" usemap="#map" />'+
+'  <img src="1S-P10.png" usemap="#map" />'+
+''+
+'<map name="map">'+
+'<area shape="poly" coords="153,358,140,327,150,306,175,300,193,314,189,334,184,352,198,356,207,370,212,402,230,366,246,309,249,277,238,257,244,252,266,265,276,259,273,273,273,284,266,291,272,317,268,343,260,366,261,378,277,375,336,444,365,460,310,460,271,421,254,460,186,459,172,450,161,430,152,460,116,460,130,438,135,390,139,369,159,361" href="applejack/" title="Applejack" />'+
+'<area shape="poly" coords="432,460,427,447,389,430,362,420,406,396,409,387,423,382,454,344,532,384,561,395,567,382,528,356,468,298,452,292,466,277,506,268,516,234,500,188,462,176,428,187,410,208,414,239,431,254,434,255,429,265,429,276,423,280,393,271,378,276,294,315,287,316,287,341,307,339,375,308,371,340,353,347,292,393,307,407,301,425,321,436,336,443,358,458,387,460" href="bow_tie" title="Bow Tie" />'+
+'<area shape="poly" coords="482,459,501,447,509,390,531,388,518,364,489,342,480,338,476,370,453,374,443,387,451,420,462,426,466,443,465,457" href="cheerilee/" title="Cheerilee" />'+
+'<area shape="poly" coords="598,260,576,211,562,213,554,232,530,250,522,255,484,259,487,266,544,264,532,284,526,304,561,311,563,286,574,267,598,274" href="cherries_jubilee/" title="Cherries Jubilee" />'+
+'<area shape="poly" coords="502,255,515,257,528,252,524,224,533,230,552,204,553,187,537,185,529,191,518,160,485,179,491,201,488,212,483,234,494,235,503,231,500,252,499,252,500,255" href="locket/" title="Locket" />'+
+'<area shape="rect" coords="329,205,376,285" href="magic_star/" title="Magic Star" />'+
+'<area shape="poly" coords="47,460,86,363,108,388,121,418,131,420,129,444,103,447,71,460" href="meadowlark_bloom/" title="Meadowlark Bloom" />'+
+'<area shape="poly" coords="251,247,250,235,232,223,228,191,243,191,246,178,243,171,220,157,201,163,194,172,206,182,188,216,187,240,204,292,206,310,189,351,207,366,213,401,233,361,246,309,248,276,238,255" href="meadowlark_bloom/" title="Meadowlark Bloom" />'+
+'<area shape="poly" coords="160,460,154,448,161,428,170,442,175,451,193,460" href="meadowlark_bloom/" title="Meadowlark Bloom" />'+
+'<area shape="poly" coords="50,154,38,161,51,245,68,296,56,354,24,412,31,434,64,444,85,441,113,433,122,425,122,395,101,405,115,375,122,359,147,339,144,322,154,307,167,302,141,260,154,226,126,200,92,210,92,242,97,254,81,240,70,201,61,155" href="princess_starburst/" title="Princess Starburst" />'+
+'<area shape="poly" coords="113,28,77,62,89,78,138,60,158,90,154,102,167,105,181,91,176,67,242,69,283,42,323,0,196,0,178,12,171,26,147,19,111,31" href="skydancer/" title="Skydancer" />'+
+'<area shape="rect" coords="275,36,332,106" href="truly/" title="Truly" />'+
+'</map>'+
+'';
+
+var pl = document.getElementById('puzzle-list');
+pl.innerHTML = ''+
+'  <tr>\n'+
+'    <td>\n'+
+'      <a href="applejack/" class="unlocked">Applejack</a>\n'+
+'    </td>\n'+
+'    <td>\n'+
+'      <a href="bow_tie/" class="unlocked">Bow Tie</a>\n'+
+'    </td>\n'+
+'  </tr>\n'+
+'\n'+
+'  <tr>\n'+
+'    <td>\n'+
+'      <a href="cheerilee/" class="unlocked">Cheerilee</a>\n'+
+'    </td>\n'+
+'    <td>\n'+
+'      <a href="cherries_jubilee/" class="unlocked">Cherries Jubilee</a>\n'+
+'    </td>\n'+
+'  </tr>\n'+
+'\n'+
+'  <tr>\n'+
+'    <td>\n'+
+'      <a href="locket/" class="unlocked">Locket</a>\n'+
+'    </td>\n'+
+'    <td>\n'+
+'      <a href="magic_star/" class="unlocked">Magic Star</a>\n'+
+'    </td>\n'+
+'  </tr>\n'+
+'\n'+
+'  <tr>\n'+
+'    <td>\n'+
+'      <a href="meadowlark_bloom/" class="unlocked">Meadowlark Bloom</a>\n'+
+'    </td>\n'+
+'    <td>\n'+
+'      <a href="princess_starburst/" class="unlocked">Princess Starburst</a>\n'+
+'    </td>\n'+
+'  </tr>\n'+
+'\n'+
+'  <tr>\n'+
+'    <td>\n'+
+'      <a href="skydancer/" class="unlocked">Skydancer</a>\n'+
+'    </td>\n'+
+'    <td>\n'+
+'      <a href="truly/" class="unlocked">Truly</a>\n'+
+'    </td>\n'+
+'  </tr>\n'+
+'';
