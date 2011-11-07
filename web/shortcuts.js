2c6047ded1ae2ec619d7a55f20f6fcf713042e2c
C. Scott Ananian <cscott@cscott.net>
2011-11-07 03:27:14 -0500

    Reuse shortcuts style & html.  Split off 1C stylesheet.

---
+++ web/shortcuts.js
@@ -0,0 +1,20 @@
+this.depth=this.depth || '../';
+var shortcuts=document.createElement('div');
+shortcuts.innerHTML=''+
+'<div id="shortcuts" href="#">Shortcuts'+
+'<ul class="menu">'+
+'  <li><a href="'+this.depth+'">Home</a></li>'+
+'  <li><a href="'+this.depth+'main/intro/">Introduction</a></li>'+
+'  <li><a href="#">Contact HQ</a></li>'+
+'  <li><a href="#">Events</a></li>'+
+'  <li><a href="#">Answer Board</a></li>'+
+'  <li><a href="#">Achievements</a></li>'+
+'  <li><a href="#">Updates</a></li>'+
+'</ul>'+
+'</div>'+
+'<a id="check-answer" href="'+this.depth+'check-answer.cgi">Call in answer</a>'+
+'';
+var checkAnswer = shortcuts.children[1];
+shortcuts = shortcuts.children[0];
+document.body.appendChild(shortcuts);
+document.body.appendChild(checkAnswer);
