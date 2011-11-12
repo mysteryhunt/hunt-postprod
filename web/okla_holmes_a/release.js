85023f9cd7813c694f182973ccba5b95e30baf00
C. Scott Ananian <cscott@cscott.net>
2011-11-12 13:16:35 -0500

    First draft of Okla-Holmes-a!

---
+++ web/okla_holmes_a/release.js
@@ -0,0 +1,10 @@
+var ii = document.getElementById('image-image');
+ii.innerHTML = ''+
+''+
+'<map name="map">'+
+'</map>'+
+'';
+
+var pl = document.getElementById('puzzle-list');
+pl.innerHTML = ''+
+    '';
