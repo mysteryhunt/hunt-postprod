14434122e338292f44f241ae977be4dbf527a66a
C. Scott Ananian <cscott@cscott.net>
2011-12-07 00:26:27 -0500

    Add some area-split functionality to mkimagemap.py to handle diagonals better.

--- mkimagemap.py
+++ mkimagemap.py
@@ -5,6 +5,8 @@ import itertools
 import png
 import sys
 
+SPLIT_DIAGONALS=3
+
 def transpose(points):
     return [(y,x) for (x,y) in points]
 
@@ -39,6 +41,35 @@ def divide(points):
         return y1,y2,yamt
     else:
         return points,[],0
+
+def splity(points):
+    a,b,amt = splitx(transpose(points))
+    return transpose(a), transpose(b), amt
+
+def compute_area(points):
+    maxx = max(x for x,y in points)
+    minx = min(x for x,y in points)
+    maxy = max(y for x,y in points)
+    miny = min(y for x,y in points)
+    return (1+maxx-minx) * (1+maxy-miny)
+
+def splitx(points):
+    maxx = max(x for x,y in points)
+    minx = min(x for x,y in points)
+    best = None
+    for mid in xrange(minx, maxx):
+        np1 = [(x,y) for x,y in points if x <= mid]
+        np2 = [(x,y) for x,y in points if x >  mid]
+        area = compute_area(np1) + compute_area(np2)
+        if best is None or area < best[2]:
+            best = (np1, np2, area)
+    return (points, [], compute_area(points)) if best is None else best
+
+def split(points):
+    x1,x2,xarea = splitx(points)
+    y1,y2,yarea = splity(points)
+    if xarea < yarea: return [x1, x2]
+    else: return [y1, y2]
 
 def area_elem(points, url, alt_text):
     maxx = max(x for x,y in points)
@@ -85,6 +116,12 @@ def mkimagemap(filename, url, alt_text):
                 inp.append(a)
             if len(b) > 10:
                 inp.append(b)
+    # sort pieces
+    if SPLIT_DIAGONALS > 0 and len(outp) > 0:
+        for i in xrange(SPLIT_DIAGONALS):
+            outp.sort(key=lambda p: len(p))
+            outp = split(outp[0]) + outp[1:]
+    # emit <area> tags
     for p in outp:
         print area_elem(p, url, alt_text)
 
