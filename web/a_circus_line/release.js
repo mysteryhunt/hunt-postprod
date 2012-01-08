804ffc20c8fdadbe7190c0f358dc81a5e02d30e3
C. Scott Ananian <cscott@cscott.net>
2012-01-08 10:06:17 -0500

    New puzzle from DB: Blackout

--- web/a_circus_line/release.js
+++ web/a_circus_line/release.js
@@ -4,14 +4,14 @@ function imagemap() {
 "<img src=\"1207_1370/-"+
 (puzzle_solved["1207_1370"]?"solved":"unsolved")+
 ".png\" />"+
+"<img src=\"blackout/-"+
+(puzzle_solved["blackout"]?"solved":"unsolved")+
+".png\" />"+
 "<img src=\"fight_choreography/-"+
 (puzzle_solved["fight_choreography"]?"solved":"unsolved")+
 ".png\" />"+
 "<img src=\"good_auspicious_hunting/-"+
 (puzzle_solved["good_auspicious_hunting"]?"solved":"unsolved")+
-".png\" />"+
-"<img src=\"magic_star/-"+
-(puzzle_solved["magic_star"]?"solved":"unsolved")+
 ".png\" />"+
 "<img src=\"middle_see/-"+
 (puzzle_solved["middle_see"]?"solved":"unsolved")+
@@ -34,9 +34,9 @@ function imagemap() {
 "<img src=\"../1px.png\" usemap=\"#map\" style=\"z-index:99\" />"+
 "<map name=\"map\">"+
 "<area shape=\"rect\" coords=\"384,321,539,348\" alt=\"1207 1370\" title=\"1207 1370\" href=\"1207_1370/\" />\n<area shape=\"rect\" coords=\"336,349,583,395\" alt=\"1207 1370\" title=\"1207 1370\" href=\"1207_1370/\" />\n<area shape=\"rect\" coords=\"480,209,611,320\" alt=\"1207 1370\" title=\"1207 1370\" href=\"1207_1370/\" />\n<area shape=\"rect\" coords=\"341,455,497,537\" alt=\"1207 1370\" title=\"1207 1370\" href=\"1207_1370/\" />\n<area shape=\"rect\" coords=\"353,396,649,454\" alt=\"1207 1370\" title=\"1207 1370\" href=\"1207_1370/\" />"+
+"<area shape=\"rect\" coords=\"56,238,170,397\" alt=\"Blackout\" title=\"Blackout\" href=\"blackout/\" />\n<area shape=\"rect\" coords=\"50,398,163,446\" alt=\"Blackout\" title=\"Blackout\" href=\"blackout/\" />\n<area shape=\"rect\" coords=\"31,447,142,517\" alt=\"Blackout\" title=\"Blackout\" href=\"blackout/\" />\n<area shape=\"rect\" coords=\"47,183,77,237\" alt=\"Blackout\" title=\"Blackout\" href=\"blackout/\" />\n<area shape=\"rect\" coords=\"171,316,191,399\" alt=\"Blackout\" title=\"Blackout\" href=\"blackout/\" />"+
 "<area shape=\"rect\" coords=\"575,215,602,240\" alt=\"Fight Choreography\" title=\"Fight Choreography\" href=\"fight_choreography/\" />\n<area shape=\"rect\" coords=\"618,222,639,240\" alt=\"Fight Choreography\" title=\"Fight Choreography\" href=\"fight_choreography/\" />\n<area shape=\"rect\" coords=\"569,241,632,258\" alt=\"Fight Choreography\" title=\"Fight Choreography\" href=\"fight_choreography/\" />\n<area shape=\"rect\" coords=\"584,272,609,300\" alt=\"Fight Choreography\" title=\"Fight Choreography\" href=\"fight_choreography/\" />\n<area shape=\"rect\" coords=\"566,259,623,271\" alt=\"Fight Choreography\" title=\"Fight Choreography\" href=\"fight_choreography/\" />"+
 "<area shape=\"rect\" coords=\"561,397,577,531\" alt=\"Good Auspicious Hunting\" title=\"Good Auspicious Hunting\" href=\"good_auspicious_hunting/\" />\n<area shape=\"rect\" coords=\"578,406,588,497\" alt=\"Good Auspicious Hunting\" title=\"Good Auspicious Hunting\" href=\"good_auspicious_hunting/\" />\n<area shape=\"rect\" coords=\"541,433,560,536\" alt=\"Good Auspicious Hunting\" title=\"Good Auspicious Hunting\" href=\"good_auspicious_hunting/\" />\n<area shape=\"rect\" coords=\"589,414,617,451\" alt=\"Good Auspicious Hunting\" title=\"Good Auspicious Hunting\" href=\"good_auspicious_hunting/\" />\n<area shape=\"rect\" coords=\"522,438,540,487\" alt=\"Good Auspicious Hunting\" title=\"Good Auspicious Hunting\" href=\"good_auspicious_hunting/\" />"+
-"<area shape=\"rect\" coords=\"56,238,170,397\" alt=\"Magic Star\" title=\"Magic Star\" href=\"magic_star/\" />\n<area shape=\"rect\" coords=\"50,398,163,446\" alt=\"Magic Star\" title=\"Magic Star\" href=\"magic_star/\" />\n<area shape=\"rect\" coords=\"31,447,142,517\" alt=\"Magic Star\" title=\"Magic Star\" href=\"magic_star/\" />\n<area shape=\"rect\" coords=\"47,183,77,237\" alt=\"Magic Star\" title=\"Magic Star\" href=\"magic_star/\" />\n<area shape=\"rect\" coords=\"171,316,191,399\" alt=\"Magic Star\" title=\"Magic Star\" href=\"magic_star/\" />"+
 "<area shape=\"rect\" coords=\"402,242,415,308\" alt=\"Middle-See\" title=\"Middle-See\" href=\"middle_see/\" />\n<area shape=\"rect\" coords=\"406,309,415,315\" alt=\"Middle-See\" title=\"Middle-See\" href=\"middle_see/\" />\n<area shape=\"rect\" coords=\"416,244,432,331\" alt=\"Middle-See\" title=\"Middle-See\" href=\"middle_see/\" />\n<area shape=\"rect\" coords=\"385,276,398,297\" alt=\"Middle-See\" title=\"Middle-See\" href=\"middle_see/\" />\n<area shape=\"rect\" coords=\"399,261,401,285\" alt=\"Middle-See\" title=\"Middle-See\" href=\"middle_see/\" />"+
 "<area shape=\"rect\" coords=\"155,462,359,502\" alt=\"Potlines\" title=\"Potlines\" href=\"potlines/\" />\n<area shape=\"rect\" coords=\"155,503,362,505\" alt=\"Potlines\" title=\"Potlines\" href=\"potlines/\" />\n<area shape=\"rect\" coords=\"165,295,321,437\" alt=\"Potlines\" title=\"Potlines\" href=\"potlines/\" />\n<area shape=\"rect\" coords=\"138,506,415,537\" alt=\"Potlines\" title=\"Potlines\" href=\"potlines/\" />\n<area shape=\"rect\" coords=\"159,438,343,461\" alt=\"Potlines\" title=\"Potlines\" href=\"potlines/\" />"+
 "<area shape=\"rect\" coords=\"243,0,363,6\" alt=\"Pure and Simple\" title=\"Pure and Simple\" href=\"pure_and_simple/\" />\n<area shape=\"rect\" coords=\"212,7,366,27\" alt=\"Pure and Simple\" title=\"Pure and Simple\" href=\"pure_and_simple/\" />\n<area shape=\"rect\" coords=\"201,45,325,79\" alt=\"Pure and Simple\" title=\"Pure and Simple\" href=\"pure_and_simple/\" />\n<area shape=\"rect\" coords=\"100,25,200,120\" alt=\"Pure and Simple\" title=\"Pure and Simple\" href=\"pure_and_simple/\" />\n<area shape=\"rect\" coords=\"205,28,349,44\" alt=\"Pure and Simple\" title=\"Pure and Simple\" href=\"pure_and_simple/\" />"+
@@ -57,25 +57,25 @@ function puzzlelist() {
 "\">Potlines</a></td>"+
 "</tr>"+
 "<tr>"+
-"<td><a href=\"fight_choreography/\" class=\""+
-(puzzle_solved["fight_choreography"]?"solved":"unsolved")+
-"\">Fight Choreography</a></td>"+
+"<td><a href=\"blackout/\" class=\""+
+(puzzle_solved["blackout"]?"solved":"unsolved")+
+"\">Blackout</a></td>"+
 "<td><a href=\"pure_and_simple/\" class=\""+
 (puzzle_solved["pure_and_simple"]?"solved":"unsolved")+
 "\">Pure and Simple</a></td>"+
 "</tr>"+
 "<tr>"+
-"<td><a href=\"good_auspicious_hunting/\" class=\""+
-(puzzle_solved["good_auspicious_hunting"]?"solved":"unsolved")+
-"\">Good Auspicious Hunting</a></td>"+
+"<td><a href=\"fight_choreography/\" class=\""+
+(puzzle_solved["fight_choreography"]?"solved":"unsolved")+
+"\">Fight Choreography</a></td>"+
 "<td><a href=\"revisiting_history/\" class=\""+
 (puzzle_solved["revisiting_history"]?"solved":"unsolved")+
 "\">Revisiting History</a></td>"+
 "</tr>"+
 "<tr>"+
-"<td><a href=\"magic_star/\" class=\""+
-(puzzle_solved["magic_star"]?"solved":"unsolved")+
-"\">Magic Star</a></td>"+
+"<td><a href=\"good_auspicious_hunting/\" class=\""+
+(puzzle_solved["good_auspicious_hunting"]?"solved":"unsolved")+
+"\">Good Auspicious Hunting</a></td>"+
 "<td><a href=\"star_search/\" class=\""+
 (puzzle_solved["star_search"]?"solved":"unsolved")+
 "\">Star Search</a></td>"+
