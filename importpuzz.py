dc375f74ee07f5034a54ea5bf3324b25f3426adc
C. Scott Ananian <cscott@cscott.net>
2012-01-07 21:51:45 -0500

    Don't protect &#39; in puzzle titles.

--- importpuzz.py
+++ importpuzz.py
@@ -142,8 +142,9 @@ def do_import_of_zf(zf, root_dir, round_name, authors,
         if not s.startswith("<!DOCTYPE"):
             s = "<!DOCTYPE HTML>\n" + s
             lineoffset = 1
-        # HACK TO PROTECT &#39;
+        # HACK TO PROTECT &#39; (except in titles)
         s = re.sub(r'(&#[0-9]+;)',r'<tt class="ponypony">\1</tt>', s)
+        s = re.sub(r'(<title>.*)<tt class="ponypony">([^<]+)</tt>(.*</title>)',r'\1\2\3', s)
         parser = html5lib.HTMLParser(
             tree=html5lib.treebuilders.getTreeBuilder("dom"))
         doc = parser.parse(s)
