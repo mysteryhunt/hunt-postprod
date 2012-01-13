661b7b4343c63c70047c262431bc7865c326f91f
C. Scott Ananian <cscott@cscott.net>
2012-01-13 16:10:32 -0500

    Second critic letter.

--- web/letters_from_max_and_leo/release.js
+++ web/letters_from_max_and_leo/release.js
@@ -1,3 +1,5 @@
 this.letters_from_max_and_leo = [
-    ['Greetings from Max', 'greetings_from_max']
+    ['Greetings from Max', 'greetings_from_max'],
+    ['Betsy Johnson', 'betsy_johnson'],
+    ['Charles Lutwidge Dodgson', 'charles_lutwidge_dodgson']
 ];
