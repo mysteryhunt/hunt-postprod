e1e1fd9f1519bf37eb5add02d87fe1b547768e86
C. Scott Ananian <cscott@cscott.net>
2012-01-13 17:01:52 -0500

    Two more letters from Max and Leo.

--- web/letters_from_max_and_leo/release.js
+++ web/letters_from_max_and_leo/release.js
@@ -1,5 +1,11 @@
 this.letters_from_max_and_leo = [
     ['Greetings from Max', 'greetings_from_max'],
     ['Betsy Johnson', 'betsy_johnson'],
-    ['Charles Lutwidge Dodgson', 'charles_lutwidge_dodgson']
+    ['Charles Lutwidge Dodgson', 'charles_lutwidge_dodgson'],
+    ['William S. Bergman', 'william_s_bergman'],
+    ['Ben Bitdiddle', 'ben_bitdiddle'],
+    ['Sheila Sunshine', 'sheila_sunshine'],
+    ['Watson 2.0', 'watson_2_0'],
+    ["Let's Put on a Hit!", 'lets_put_on_a_hit'],
+    ["Password Reminder", 'password_reminder']
 ];
