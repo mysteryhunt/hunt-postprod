1d2e31bf4a1fc366a8f0192fa9f93280d15a7bd4
C. Scott Ananian <cscott@cscott.net>
2012-01-12 21:33:48 -0500

    New puzzle: March Madness.

---
+++ web/betsy_johnson/march_madness/brackets.js
@@ -0,0 +1,464 @@
+this.SERVER_BASE = this.SERVER_BASE || 'https://mysteryhunt2012.appspot.com';
+this.PUZZLE_NAME = "march_madness";
+this.PUZZLE_AUTH = "96be150d4b66374215f989c37fea3ea2058ce553";
+
+var Game = function(id, parent, bracketNum, attachmentNode) {
+    this.parent     = parent;
+    this.teams  = [null, null];
+    this.id = id;
+    this.winner = null;
+    this.fixed  = false;
+    this.bracketNum = bracketNum;
+    this.teamsdivs  = [];
+    this.gameContainer = attachmentNode;
+
+    for (var idex = 0; idex < 2; ++idex)
+    {
+        var self    = this;
+        this.teamsdivs[idex]   = document.createElement("div");
+        $(this.teamsdivs[idex]).addClass("team mutable loc"+idex);
+        var span = document.createElement("span");
+        $(span).addClass("name_container");
+        this.teamsdivs[idex].appendChild(span);
+
+        function setListener(jdex)
+        {
+            self.teamsdivs[idex].addEventListener("click",
+                                                  function(event)
+                                                  {
+                                                    self.toggleWinner(jdex);
+                                                  },
+                                                  false);
+        }
+
+        setListener(idex);
+
+        attachmentNode.appendChild(this.teamsdivs[idex]);
+    }
+};
+
+Game.prototype = {
+    toggleWinner: function(index)
+    {
+        if (this.fixed) return;
+        if (!this.teams[index]) return;
+        if (!this.teams[index^1]) return;
+
+        if (this.winner == index)
+        {
+                this.winner = null;
+                this.parent.unsetTeam(this.teams[index], this.bracketNum & 1, this.fixed);
+                $(this.teamsdivs[index]).removeClass("selected");
+        }
+        else
+        {
+            if (this.winner !== null)
+            {
+                this.parent.unsetTeam(this.teams[this.winner], this.bracketNum & 1);
+            }
+
+            this.winner = index;
+            this.parent.setTeam(this.teams[index], this.bracketNum & 1, this.fixed);
+            $(this.teamsdivs[index]).addClass("selected");
+            $(this.teamsdivs[index ^ 1]).removeClass("selected");
+        }
+        this.parent.checkMutability();
+    },
+
+    checkMutability: function() {
+        var mutable = (this.teams[0] && this.teams[1] && !this.fixed);
+        $(this.gameContainer).toggleClass("mutable", !!mutable);
+    },
+
+
+    fixWinner:  function(index)
+    {
+        var self    = this;
+        if (this.winner !== index) this.toggleWinner(index);
+        this.fixed  = true;
+
+        $(this.teamsdivs[0]).addClass("fixed");
+        $(this.teamsdivs[1]).addClass("fixed");
+        this.checkMutability();
+
+        this.parent.setTeam(this.teams[index], this.bracketNum & 1, this.fixed);
+    },
+
+    setTeam:    function(teamName, index, fixedfont)
+    {
+        this.teams[index]   = teamName;
+        this.teamsdivs[index].firstChild.innerHTML = teamName;
+
+        $(this.teamsdivs[index]).toggleClass("fixed-font", !!fixedfont);
+    },
+
+    unsetTeam:  function(teamName, index)
+    {
+        if (this.winner != null)
+        {
+            this.toggleWinner(this.winner);
+        }
+
+        this.teams[index]   = null;
+        this.teamsdivs[index].firstChild.innerHTML  = "";
+    }
+};
+
+var Bracket = function(games, round5set)
+{
+    this.games  = games;
+    this.round5set = round5set;
+    // sort list of games
+    this.sortedGames = [];
+    for (var id in games) {
+        this.sortedGames.push(id);
+    }
+    this.sortedGames.sort();
+}
+
+Bracket.prototype   =
+{
+    checkFilledBracket: function() {
+        checkFilledBracket(this.round5set);
+    },
+    isFilled: function() {
+        return this.round5set[0] && this.round5set[1] &&
+            this.round5set[2] && this.round5set[3];
+    },
+    to_string:  function()
+    {
+        var winners = {};
+
+        for (var idex = 0; idex < this.games.length; ++idex)
+        {
+            winners[this.games[idex].id] = this.games[idex].winner;
+        }
+
+        return JSON.stringify(winners);
+    }
+}
+
+var clock_timer = null;
+
+/* Width calculations:
+  Games currently have width 120
+  Games currently have height 40
+  Vertical space between round 2 games is 20; subsequent rounds should be vertically aligned to match their child games
+  Horizontal space between rounds is 50
+*/
+var game_width = 130;
+var game_height = 40;
+var horizontal_space = 50;
+var vertical_space = 20;
+var extra_vertical_space = 130;
+//((130 + 50) * 3 + 720) = 180 * 3 + 720 = 1260
+function pad(width, n) {
+    var r = '' + n;
+    while (r.length < width) {
+        r = '0' + r;
+    }
+    return r;
+}
+function    addGame(games, round, index)
+{
+    var id  = "round" + round + "_game" + pad(2, index);
+    var parent_id   = "round" + (round + 1) + "_game" + pad(2, (index >> 1));
+    var parent_node = document.getElementById(id);
+
+    if (round > 0)
+    {
+        var top = 0;
+        var left = 0;
+
+        if (index < (1 << (5 - round)))
+        {
+            top   = (((game_height + vertical_space) * (index + 1) - 30) << (round - 1)) + extra_vertical_space;
+            left  = (game_width + horizontal_space) * (round - 1) + 10;
+        }
+        else
+        {
+            top   = (game_height + vertical_space) * ((((index - (1 << (5 - round))) + 1)) << (round - 1)) - (30 << (round - 1)) + extra_vertical_space;
+            left = (game_width + horizontal_space) * (4 - round) + 720;
+        }
+
+        if (((index >> (4 - round)) & 1) == 1)
+        {
+            top += vertical_space;
+        }
+
+        parent_node.style.top = "" + top + "px";
+        parent_node.style.left = "" + left + "px";
+
+        if (round < 4)
+        {
+            var connector_node  = document.getElementById("round" + round + "_index" + (index >> 1) + "_connector");
+
+            if (index < (1 << (5 - round)))
+            {
+                top   = (game_height + vertical_space) * ((index + 1) << (round - 1)) - (90 << (round - 1)) + extra_vertical_space + 25;
+                left  = (game_width + horizontal_space) * (round - 1) + 147;
+            }
+            else
+            {
+                top   = (game_height + vertical_space) * (((index - (1 << (5 - round))) + 1) << (round - 1)) - (90 << (round - 1)) + extra_vertical_space + 25;
+                left  = (game_width + horizontal_space) * (4 - round) + 677;
+            }
+
+            if (((index >> (4 - round)) & 1) == 1)
+            {
+                top += vertical_space;
+            }
+
+            connector_node.style.top = "" + top + "px";
+            connector_node.style.left = "" + left + "px";
+            connector_node.style.height = 60 << (round - 1);
+        }
+    }
+
+    games[id]  = new Game(id, games[parent_id], index, parent_node);
+}
+
+function checkFilledBracket(round5set)
+{
+    var ok = true;
+    if (round5set[0] || round5set[1] || round5set[2] || round5set[3]) {
+        $('#complete_message').html('');
+    } else {
+        $('#complete_message').html('At least one regional must be complete before submission.');
+        ok = false;
+    }
+    if (ok) {
+        $('#submitter button').removeAttr('disabled');
+    } else {
+        $('#submitter button').attr('disabled', 'disabled');
+    }
+    $('#server_message').html('');
+}
+
+function mkround5(round5set, r5i) {
+    return {
+    setTeam: function(name, index, fixedfont)
+    {
+        //console.assert(index==0);
+        var teamdiv = $("#round5_game"+pad(2,r5i)+"_team");
+        teamdiv.html(name);
+        round5set[r5i] = true;
+        checkFilledBracket(round5set);
+
+        teamdiv.toggleClass("fixed-font", !!fixedfont);
+    },
+    unsetTeam: function(name, index)
+    {
+        var teamdiv = $("#round5_game"+pad(2,r5i)+"_team");
+        teamdiv.html('');
+        round5set[r5i] = false;
+        checkFilledBracket(round5set);
+    },
+    checkMutability: function() {
+        /* do nothing */
+    },
+    };
+}
+
+function init_games() {
+    var games   = {};
+    for (var round = 4; round > 0; --round) {
+        for (var idex = 0; idex < (1 << (6 - round)); ++idex) {
+            addGame(games, round, idex);
+        }
+    }
+
+    addGame(games, 0, 7);
+    addGame(games, 0, 24);
+    addGame(games, 0, 54);
+    addGame(games, 0, 42);
+
+    var round5set = [false, false, false, false];
+    games["round4_game00"].parent = mkround5(round5set, 0);
+    games["round4_game01"].parent = mkround5(round5set, 1);
+    games["round4_game02"].parent = mkround5(round5set, 2);
+    games["round4_game03"].parent = mkround5(round5set, 3);
+    return [games, round5set];
+}
+
+function init_bracket(games, round5set) {
+    var team_names  = {
+    /* Round 0 */
+    "round0_game07":  ["Northeastern", "Texas"],
+    "round0_game24":  ["IPFW", "Portland State"],
+    "round0_game54":  ["Montana", "Southern Utah"],
+    "round0_game42":  ["Georgia Tech", "Tulsa"],
+
+    /* Round 1 */
+    /* Division 1 */
+    "round1_game00":  ["Albany", "Drexel"],
+    "round1_game01":  ["Rhode Island", "Santa Clara"],
+    "round1_game02":  ["Florida A&M", "Jacksonville State"],
+    "round1_game03":  ["Princeton"],
+    "round1_game04":  ["Arkansas", "VCU"],
+    "round1_game05":  ["Coastal Carolina", "SIU-Carbondale"],
+    "round1_game06":  ["Delaware", "Idaho State"],
+    "round1_game07":  ["SMU", "South Dakota State"],
+
+    /* Division 2 */
+    "round1_game08":  ["Austin Peay", "Tennessee"],
+    "round1_game09":  ["Duke", "Oklahoma"],
+    "round1_game10":  ["Cleveland State", "GWU"],
+    "round1_game11":  ["La Salle", "San Jose State"],
+    "round1_game12":  [null, "Xavier"],
+    "round1_game13":  ["Manhattan", "UALR"],
+    "round1_game14":  ["Long Beach State", "San Diego State"],
+    "round1_game15":  ["Massachusetts", "Notre Dame"],
+
+    /* Division 3 */
+    "round1_game16":  ["Alabama", "Loyola (Maryland)"],
+    "round1_game17":  ["Middle Tennessee", "North Texas"],
+    "round1_game18":  ["Army", "Rutgers"],
+    "round1_game19":  ["Cornell", "Dartmouth"],
+    "round1_game20":  ["DePaul", "Evansville"],
+    "round1_game21":  [null, "Tulane"],
+    "round1_game22":  ["LIU", "Minnesota"],
+    "round1_game23":  ["Stanford", "Syracuse"],
+
+    /* Division 4 */
+    "round1_game24":  ["Alabama State", "Toledo"],
+    "round1_game25":  ["Stony Brook", "UAB"],
+    "round1_game26":  ["Marshall", "Ohio State"],
+    "round1_game27":  [null, "Virginia"],
+    "round1_game28":  ["Arkansas State", "South Florida"],
+    "round1_game29":  ["Montana State", "Oakland"],
+    "round1_game30":  ["Buffalo", "Saint Joseph's"],
+    "round1_game31":  ["Sacramento State", "UT Arlington"],
+    };
+    for (var id in team_names) {
+        games[id].setTeam(team_names[id][0], 0, true);
+        games[id].setTeam(team_names[id][1], 1, true);
+    }
+
+    for (var id in games) {
+        games[id].checkMutability();
+    }
+
+    return new Bracket(games, round5set);
+}
+
+var bracket  = init_bracket.apply(this, init_games());
+var clock = document.getElementById("clock");
+
+function set_clock(seconds)
+{
+    var time_str =
+        pad(2, Math.floor(seconds / 60)) +
+        pad(2, Math.ceil(seconds) % 60);
+
+    for (var i=0; i<time_str.length; i++)
+        $('#clock'+i).attr('src', time_str[i]+'.jpg');
+
+    if (seconds > 0)
+    {
+        $('#clock').removeClass('zero');
+        clock_timer = setTimeout(function(){set_clock(seconds - 1);}, 1000);
+    }
+    else
+    {
+        $('#clock').addClass('zero');
+        clock_timer = null;
+        bracket.checkFilledBracket();
+    }
+}
+
+function set_state(state)
+{
+    $('body').removeClass('loading');
+
+    if (state.clock !== undefined)
+    {
+        if (clock_timer)
+        {
+            clearTimeout(clock_timer);
+        }
+        set_clock(state.clock);
+    }
+
+    if (state.winners) {
+        for (var i=0; i<state.winners.length; i++) {
+            var winner = state.winners[i];
+            var game = bracket.games[winner.id];
+            if (winner.fixed) {
+                game.fixWinner(winner.winner);
+            } else if (game.winner !== winner.winner) {
+                game.toggleWinner(winner.winner);
+            }
+        }
+    }
+    bracket.checkFilledBracket(); // ...and clock
+
+    if (state.error) {
+        $('#server_message').html(state.error);
+    }
+    if (state.message) {
+        $('#bracket').hide();
+        $('#message2').html(state.message);
+    }
+}
+
+function get_winner(round, game)
+{
+  if (game < 10) { game = "0" + game; }
+  winner = bracket.games["round" + round + "_game" + game].winner;
+  if (winner !== null) {
+    return winner;
+  } else {
+     return "9";
+  }
+}
+
+function get_state()
+{
+    var retVal  = [];
+
+    for (var region=0; region<4; region++) {
+        region_result = "";
+        for (var i=0; i<8; i++) {
+          game = 8*region + i;
+          region_result += get_winner(1, game);
+        }
+        for (var i=0; i<4; i++) {
+          game = 4*region + i;
+          region_result += get_winner(2, game);
+        }
+        for (var i=0; i<2; i++) {
+          game = 2*region + i;
+          region_result += get_winner(3, game);
+        }
+        for (var i=0; i<1; i++) {
+          game = 1*region + i;
+          region_result += get_winner(4, game);
+        }
+        
+        retVal.push({r:region, result:region_result});
+    }
+
+    return {winners:retVal};
+}
+
+function framework_ready(_server) {
+    $("#submitter button").click(function(event) {
+        if (clock_timer) {
+            set_state({error: "Too soon since your last submission."});
+            return;
+        }
+        var c = "Are you sure you want to submit this bracket?";
+        if (!bracket.isFilled()) {
+            c += "  Your bracket is not complete.";
+        }
+        if (confirm(c))
+        {
+            $('body').addClass('loading');
+            _server.query(get_state(), set_state);
+        }
+    });
+    _server.query("reset", set_state);
+}
+
+$.getScript(this.SERVER_BASE);
---
+++ web/betsy_johnson/march_madness/solution/secret.js
@@ -0,0 +1,142 @@
+// this is the bit which is on a server for the actual hunt
+
+// IF YOU ARE TEST SOLVING, DON'T LOOK AT THIS.
+var DEBUG=false;
+var PROVIDE_SOLUTION=false;
+
+var SOLUTION = [
+    /* givens */
+    [{"id":"round0_game07","winner":1,"fixed":true},{"id":"round0_game24","winner":0,"fixed":true},{"id":"round0_game42","winner":0,"fixed":true},{"id":"round0_game54","winner":0,"fixed":true},{"id":"round1_game00","winner":1,"fixed":true},{"id":"round1_game01","winner":1,"fixed":true},{"id":"round1_game02","winner":0,"fixed":true},{"id":"round1_game05","winner":0,"fixed":true},{"id":"round1_game08","winner":1,"fixed":true},{"id":"round1_game09","winner":1,"fixed":true},{"id":"round1_game13","winner":1,"fixed":true},{"id":"round1_game14","winner":1,"fixed":true},{"id":"round1_game17","winner":0,"fixed":true},{"id":"round1_game18","winner":1,"fixed":true},{"id":"round1_game19","winner":1,"fixed":true},{"id":"round1_game22","winner":0,"fixed":true},{"id":"round1_game24","winner":1,"fixed":true},{"id":"round1_game26","winner":1,"fixed":true},{"id":"round1_game28","winner":1,"fixed":true},{"id":"round1_game29","winner":0,"fixed":true}],
+    /* top-left */
+    [{"id":"round1_game03","winner":1,"fixed":true},{"id":"round1_game04","winner":1,"fixed":true},{"id":"round1_game06","winner":1,"fixed":true},{"id":"round1_game07","winner":1,"fixed":true},{"id":"round2_game00","winner":0,"fixed":true},{"id":"round2_game01","winner":1,"fixed":true},{"id":"round2_game02","winner":0,"fixed":true},{"id":"round2_game03","winner":0,"fixed":true},{"id":"round3_game00","winner":1,"fixed":true},{"id":"round3_game01","winner":1,"fixed":true},{"id":"round4_game00","winner":0,"fixed":true}],
+    /* bottom-left */
+    [{"id":"round1_game10","winner":0,"fixed":true},{"id":"round1_game11","winner":1,"fixed":true},{"id":"round1_game12","winner":0,"fixed":true},{"id":"round1_game15","winner":0,"fixed":true},{"id":"round2_game04","winner":0,"fixed":true},{"id":"round2_game05","winner":1,"fixed":true},{"id":"round2_game06","winner":0,"fixed":true},{"id":"round2_game07","winner":0,"fixed":true},{"id":"round3_game02","winner":1,"fixed":true},{"id":"round3_game03","winner":0,"fixed":true},{"id":"round4_game01","winner":1,"fixed":true}],
+    /* top-right */
+    [{"id":"round1_game16","winner":1,"fixed":true},{"id":"round1_game20","winner":1,"fixed":true},{"id":"round1_game21","winner":1,"fixed":true},{"id":"round1_game23","winner":1,"fixed":true},{"id":"round2_game08","winner":0,"fixed":true},{"id":"round2_game09","winner":1,"fixed":true},{"id":"round2_game10","winner":0,"fixed":true},{"id":"round2_game11","winner":1,"fixed":true},{"id":"round3_game04","winner":0,"fixed":true},{"id":"round3_game05","winner":0,"fixed":true},{"id":"round4_game02","winner":0,"fixed":true}],
+    /* bottom-right */
+    [{"id":"round1_game25","winner":0,"fixed":true},{"id":"round1_game27","winner":1,"fixed":true},{"id":"round1_game30","winner":1,"fixed":true},{"id":"round1_game31","winner":1,"fixed":true},{"id":"round2_game12","winner":0,"fixed":true},{"id":"round2_game13","winner":1,"fixed":true},{"id":"round2_game14","winner":1,"fixed":true},{"id":"round2_game15","winner":0,"fixed":true},{"id":"round3_game06","winner":1,"fixed":true},{"id":"round3_game07","winner":1,"fixed":true},{"id":"round4_game03","winner":1,"fixed":true}]
+];
+
+var SECRET_MESSAGE = ""+
+"<p>Congratulations on your correct prediction of all the winners through the Regional Finals!</p>"+
+"<p>To win the contest, you&rsquo;ll now have to correctly predict the final scores of all three Final Four games.</p>"+
+"<p>Submit your answer to this part of the contest through the normal &ldquo;Check Answer&rdquo; link.  Good luck!</p>"+
+"<div id='finalbracket'>"+
+"<div id='SemiFinalTeam1' class='finalteam'><span class='l'>Longhorns</span><span class='blank r'>___</span></div>"+
+"<div id='SemiFinalTeam2' class='finalteam'><span class='l'>Mastodons</span><span class='blank r'>___</span></div>"+
+"<div id='SemiFinalTeam3' class='finalteam'><span class='blank l'>___</span><span class='r'>Greyhounds</span></div>"+
+"<div id='SemiFinalTeam4' class='finalteam'><span class='blank l'>___</span><span class='r'>Hawks</span></div>"+
+"<div id='FinalTeam1' class='finalteam'><span class='blank r'>___</span></div>"+
+"<div id='FinalTeam2' class='finalteam'><span class='blank l'>___</span></div>"+
+"<div id='finalLeftConnectorVertical'>"+
+"<div id='finalRightConnectorVertical'>"+
+"</div></div>"+
+"</div>"+
+"<div class='ps-note'>"+
+"P.S. A cute old lady walked by and told us that due to the changing of the guard from the rabbit to the dragon and the fact that all four teams in the Final Four are animals, it will be a piece of mooncake to foretell this year's Final Four results. She let us in on some of her divinations&hellip;"+
+"<ul>"+
+"  <li>The Greyhounds will win the championship game by 13 points.</li>"+
+"  <li>The Greyhounds&rsquo; opponents in the championship game will score the same number of points as the Greyhounds themselves will score in their semifinal game.</li>"+
+"  <li>All scores will be in the range 65&ndash;90.</li>"+
+"  <li>The Longhorns&rsquo; score in their semifinal game will be the only score in the 70s. The Greyhounds&rsquo; score in the championship game will be the only score in the 80s.</li>"+
+"  <li>The semifinal between the Greyhounds and the Hawks will go down to the wire and be decided by 1 point.</li>"+
+"  <li>The scores for the 4 teams in the semifinal games will be multiples of 4 consecutive odd prime numbers.</li>"+
+"</ul>"+
+"</div>"+
+    "";
+
+
+function dump_unfixed() {
+    var s = get_state();
+    var r = [];
+    for (var i=0; i<s.winners.length; i++) {
+        if (!s.winners[i].fixed) r.push(s.winners[i]);
+    }
+    return JSON.stringify(r);
+}
+
+var DELAY=20/*minutes*/ * 60 * 1000;
+if (DEBUG) DELAY=10*1000;
+
+function FakeServer() {
+    this.lastQuery = Date.now() - DELAY;
+    this.known = SOLUTION[0]; // givens
+    this.correct = [];
+}
+FakeServer.prototype.query = function(args, callback) {
+    var reset = false, time_error = false;
+    if (typeof(args)==="string" || !('winners' in args)) {
+        this.clear();
+        reset = true;
+    } else if ((this.lastQuery + DELAY) > Date.now()) {
+	time_error = true;
+    } else {
+        this.lastQuery = Date.now();
+        /* check submission */
+        var submitted = {};
+        for (var i=0; i<args.winners.length; i++) {
+            submitted[args.winners[i].id] = args.winners[i];
+        }
+        var knowns = [];
+        for (var i=0; i<SOLUTION.length; i++) {
+            /* do we have this whole quadrant correct? */
+            var section_right = true;
+            for (var j=0; j<SOLUTION[i].length; j++) {
+                var correct = SOLUTION[i][j];
+                if (correct.id in submitted &&
+                    submitted[correct.id].winner === correct.winner)
+                    continue; /* yay */
+                section_right = false
+                break;
+            }
+
+            if (this.correct[i] || i==0) { section_right = true; }
+            if (!section_right) continue;
+
+            /* copy correct part into knowns */
+            for (var j=0; j<SOLUTION[i].length; j++) {
+                knowns.push(SOLUTION[i][j]);
+            }
+
+            this.correct[i] = true;
+        }
+        this.known = knowns;
+    }
+    // debugging
+    if (PROVIDE_SOLUTION) {
+        this.known = [];
+        for (var i=0; i<SOLUTION.length; i++) {
+            for (var j=0; j<SOLUTION[i].length; j++) {
+                var s = SOLUTION[i][j];
+                this.known.push({id:s.id, winner:s.winner, fixed:!!(i==0)});
+            }
+        }
+	this.lastQuery = 10*1000 + Date.now() - DELAY;
+    }
+    /* return fixed part */
+    var time_left = (this.lastQuery + DELAY) - Date.now();
+    if (time_left < 0) time_left = 0;
+    var result = { winners: this.known, clock: Math.ceil(time_left/1000)};
+    /* return messages */
+    var m = [];
+    for (var i=1; i<SOLUTION.length; i++)
+        if (this.correct[i])
+            m.push(''+i);
+    if ((m.length+1) === SOLUTION.length)
+        result.message = SECRET_MESSAGE;
+    else if (m.length == 1)
+        result.error = "Regional "+m[0]+" is correct.";
+    else if (m.length > 1) {
+        var last = m.pop();
+        result.error = "Regionals "+m.join(', ')+(m.length>2?',':'')+
+            " and "+last+" are correct.";
+    } else if (m.length == 0 && !reset)
+        result.error = "None of the four regionals are correct.";
+    if (time_error)
+	result.error = "Too soon since your last submission.";
+    callback(result);
+};
+FakeServer.prototype.clear = function() {
+};
+
+framework_ready(new FakeServer());
