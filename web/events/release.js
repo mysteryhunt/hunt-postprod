dd69ef3761cdb39ba5bcc3f7eade8d4fd68a6bc5
C. Scott Ananian <cscott@cscott.net>
2012-01-14 12:39:41 -0500

    Update with the last few events.

--- web/events/release.js
+++ web/events/release.js
@@ -2,4 +2,6 @@ this.events = [
     ["Bringing Stars Together", "bringing_stars_together/"],
     ["I Can Hear You", "i_can_hear_you/"],
     ["I'm Wet!  I'm Hysterical and I’m Wet!", "im_wet_im_hysterical_and_im_wet"],
+    ["Dancing with the Vars", "dancing_with_the_vars"],
+    ["Name that Show Tune", "name_that_show_tune"]
 ];
