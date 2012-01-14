29da04ec0f50c02d86b59da3b2725fc3c95be446
C. Scott Ananian <cscott@cscott.net>
2012-01-14 09:42:56 -0500

    Updates for I Can Hear You event.

--- web/events/i_can_hear_you/solution/team-data.js
+++
@@ -1,6 +0,0 @@
-TEAM_NAME = 'codex';
-TEAM_AUTH = '6f300409744114703e1c1cf7bb09876786d19485';
-
-// Uncomment this if you want to test with a different team name
-//TEAM_NAME = 'fungus';
-//TEAM_AUTH = '8d1606460238f8cb1e1ae31a67f9cf558305c0c7';
