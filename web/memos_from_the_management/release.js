06a3c1c3f312581c8aec57377ed5bfa8ff60c269
C. Scott Ananian <cscott@cscott.net>
2012-01-14 13:46:05 -0500

    Postprod the opening skit.

--- web/memos_from_the_management/release.js
+++ web/memos_from_the_management/release.js
@@ -1,3 +1,9 @@
+---
+# ensure that we use the templating engine for this file
+---
 this.memos_from_the_management = [
-    ["Introduction to the Hunt", "introduction/"]
+    ["Introduction to the Hunt", "introduction/"],
+{% if site.release.solutions %}
+    ["Opening Skit", "skit/"]
+{% endif %}
 ];
