b8b28773a0649b568fa2cdbd9894b9305654c54b
C. Scott Ananian <cscott@cscott.net>
2012-01-13 21:42:12 -0500

    Add/announce duck event.

--- web/events/release.js
+++ web/events/release.js
@@ -1,4 +1,5 @@
 this.events = [
     ["Bringing Stars Together", "bringing_stars_together/"],
     ["I Can Hear You", "i_can_hear_you/"],
+    ["I'm Wet!  I'm Hysterical and I’m Wet!", "im_wet_im_hysterical_and_im_wet"],
 ];
